2dc10ba695782680e9cd4b28adbc0594e8e71f2f
Jobanny Zúñiga <jobannyz@gmail.com>
2021-03-25 02:40:10 +0000

    Final Update

--- src/js/component/app.js
+++ src/js/component/app.js
@@ -4,18 +4,68 @@ import { Button } from "bootstrap";
 export function ToDolist() {
 	const [list, setlist] = useState([]);
 
-	function myFunction(e) {
+	const myFunction = e => {
 		if (e.key === "Enter") {
-			setlist([...list, e.target.value]);
+			setlist([...list, { label: e.target.value, done: false }]);
 			e.target.value = "";
 		}
-	}
+	};
 
 	const DeleteItems = indexItem => {
 		setlist(prevState =>
 			prevState.filter((todo, index) => index !== indexItem)
 		);
 	};
+
+	const deleteAll = async () => {
+		await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/jobannyz",
+			{
+				method: "DELETE",
+				headers: { "Content-Type": "application/json" }
+			}
+		)
+			.then(response => response.json())
+			.then(data => console.log(data))
+			.catch(error => console.log(error));
+
+		await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/jobannyz",
+			{
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify([])
+			}
+		)
+			.then(response => response.json())
+			.then(data => console.log(data))
+			.catch(error => console.log(error));
+
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/jobannyz", {
+			method: "GET",
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(response => response.json())
+			.then(data => setlist(data));
+	};
+
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/jobannyz", {
+			method: "GET",
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(response => response.json())
+			.then(data => setlist(data));
+	}, []);
+
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/jobannyz", {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(list)
+		});
+	}, [list]);
+
 	return (
 		<div id="main" className="container">
 			<h1>Lista de Tareas</h1>
@@ -33,23 +83,30 @@ export function ToDolist() {
 							id="listitem"
 							className="list-group-item"
 							key={index}>
-							{item}{" "}
+							{item.label}{" "}
 							<i
 								className="fas fa-trash"
 								onClick={() => DeleteItems(index)}></i>
-							{/* <button className="btn btn-danger">
-								
-							</button> */}
 						</li>
 					);
 				})}
-				<div className="m-3">
-					<button
-						type="button"
-						className="btn btn-danger btn-sm text-white mx-2">
-						{list.length}
-					</button>
-					{list.length > 1 ? "items left" : "item left"}
+				<div className="container">
+					<div className="m-3 d-inline-block">
+						<button
+							type="button"
+							className="btn btn-danger btn-sm text-white mx-2">
+							{list.length}
+						</button>
+						{list.length > 1 ? "items left" : "item left"}
+					</div>
+					<div className="m-3 d-inline-block">
+						<button
+							type="button"
+							className="btn btn-warning"
+							onClick={() => deleteAll()}>
+							Borrar toda la lista!
+						</button>
+					</div>
 				</div>
 			</ul>
 		</div>
